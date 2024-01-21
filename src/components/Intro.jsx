d7891d53af75c89a54e796f4cbd8f07d3708ca6a
Lawrence Li <lawrenceli3202@gmail.com>
2024-01-20 22:12:45 -0500

    added images for projects

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,10 +4,10 @@ function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
       <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
-        Lawrence Li
+        Hi! I'm Lawrence Li
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">
-        Inspiring Software Engineer & Web Developer
+        I'm an inspiring Software Engineer & Web Developer
       </p>
       <p className="text-sm max-w-xl mb-6 font-bold">
         I'm currently a junior at Boston University studying computer science.
