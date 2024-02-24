5abd8d44d38ba3e00bf09b0484470315f2571aa6
Lawrence Li <lawrenceli3202@gmail.com>
2024-02-24 13:28:30 -0500

    Attempted to fix deploy

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,14 +16,14 @@ function Nav() {
   return (
     <>
       <div className="font-roboto bg-white">
-        {/* <div className="left-4 text-xl md:hidden pt-2 pl-2 bg-gradient-to-r from-teal-400 via-teal-300 to-teal-200 bg-clip-text text-transparent">
+        <div className="left-4 text-xl md:hidden pt-2 pl-2 bg-gradient-to-r from-teal-400 via-teal-300 to-teal-200 bg-clip-text text-transparent">
           Lawrence Li
         </div>
         <VscMenu
           onClick={handleNav}
           className="absolute top-2 right-4 z-[99] md:hidden text-2xl hover:animate-pulse hover:text-teal-200"
         />
-        {} */}
+        {}
         <nav className="md:block hidden bg-white top-0 fixed w-full">
           <div className="flex justify-between items-center h-10 p-5 shadow-lg shadow-teal-100">
             <div className="bg-gradient-to-r from-teal-400 via-teal-300 to-teal-200 bg-clip-text text-transparent text-2xl cursor-default">
