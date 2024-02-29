f0b5d588ebdd4bc1035b1f654d4d0310d6c014e6
Lawrence Li <lawrenceli3202@gmail.com>
2024-02-29 02:26:30 -0500

    Fixed custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import About from "./pages/About.jsx";
-import Contact from "./pages/Contact.jsx";
-import Home from "./pages/Home.jsx";
-import Project from "./pages/Project.jsx";
 import "./styles/tailwind.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/portfolio/",
-    element: <App />,
-    children: [
-      {
-        path: "/portfolio/",
-        element: <Home />,
-      },
-      {
-        path: "/portfolio/about",
-        element: <About />,
-      },
-      {
-        path: "/portfolio/projects",
-        element: <Project />,
-      },
-      {
-        path: "/portfolio/contact",
-        element: <Contact />,
-      },
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>
 );
