45df8cb15c0c1c0e0cb2bcea7b3f2f6e7fbe68be
Lawrence Li <lawrenceli3202@gmail.com>
2024-03-11 23:01:30 -0400

    Updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,52 @@
+import { useState } from "react";
+
 function About() {
+
+  const [displayInfo, setDisplayInfo] = useState('');
+
+  const showMeInfo = () => {
+    const meInfo =
+      <div>
+        I am a junior at Boston University studying Computer Science.
+        My main interests lie in the field of web development and software engineering,
+        with a special focus on front-end development.
+        <br /><br />
+        I am currently looking for an internship for the summer of 2024.
+        If you are interested in collaborating or have any projects you think
+        would be a good fit, feel free to reach out to me!
+      </div>;
+
+    setDisplayInfo(meInfo);
+  };
+
+  const skillsInfo = () => {
+    const meInfo =
+      <div>
+        in development
+      </div>;
+
+    setDisplayInfo(meInfo);
+  };
+
+  const eduacationInfo = () => {
+    const meInfo =
+      <div>
+        in development
+      </div>;
+
+    setDisplayInfo(meInfo);
+  };
+
+  const experienceInfo = () => {
+    const meInfo =
+      <div>
+        in development
+      </div>;
+
+    setDisplayInfo(meInfo);
+  };
+
+
   return (
     <div
       id="about"
@@ -9,22 +57,16 @@ function About() {
           About Me
         </span>
       </h1>
-      {/* <div className="flex flex-row gap-8 text-m md:text-xl">
-        <button className="bg-white p-5 rounded-3xl hover:underline hover: underline-offset-8 hover:bg-gray-500 hover:text-white hover:shadow-2xl  hover:shadow-teal-100 border-gray-500 border-2">Education</button>
-        <button className="bg-white p-5 rounded-3xl hover:underline hover: underline-offset-8 hover:bg-gray-500 hover:text-white hover:shadow-2xl  hover:shadow-teal-100 border-gray-500 border-2">Skills</button>
-        <button className="bg-white p-5 rounded-3xl hover:underline hover: underline-offset-8 hover:bg-gray-500 hover:text-white hover:shadow-2xl  hover:shadow-teal-100 border-gray-500 border-2">Experience</button>
-      </div> */}
+      <div className="flex flex-row gap-8 text-m md:text-xl">
+        <div>
+          <button className="bg-white p-5 rounded-3xl hover:underline hover: underline-offset-8 hover:bg-gray-500 hover:text-white hover:shadow-2xl  hover:shadow-teal-100 border-gray-500 border-2" onClick={showMeInfo}>Me</button>
+        </div>
+        <button className="bg-white p-5 rounded-3xl hover:underline hover: underline-offset-8 hover:bg-gray-500 hover:text-white hover:shadow-2xl  hover:shadow-teal-100 border-gray-500 border-2" onClick={eduacationInfo}>Education</button>
+        <button className="bg-white p-5 rounded-3xl hover:underline hover: underline-offset-8 hover:bg-gray-500 hover:text-white hover:shadow-2xl  hover:shadow-teal-100 border-gray-500 border-2" onClick={skillsInfo}>Skills</button>
+        <button className="bg-white p-5 rounded-3xl hover:underline hover: underline-offset-8 hover:bg-gray-500 hover:text-white hover:shadow-2xl  hover:shadow-teal-100 border-gray-500 border-2" onClick={experienceInfo}>Experience</button>
+      </div>
       <div className="flex flex-col items-center">
-        <div className="text-xl text-gray-500 pt-6">
-          I am a junior at Boston University studying Computer Science. My main
-          interests lie in the field of web development and software
-          engineering, with a special focus on front-end development.
-        </div>
-        <div className="text-xl text-gray-500 pt-6">
-          I am currently looking for an internship for the summer of 2024. If
-          you are interested in collaborating or have any projects you think
-          would be a good fit, feel free to reach out to me!
-        </div>
+        <div className="text-xl text-gray-500 pt-6">{displayInfo}</div>
       </div>
     </div>
   );
