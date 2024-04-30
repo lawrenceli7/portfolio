ee280fffa9b28ed2f71e181f877718937f371b63
Lawrence Li <lawrenceli3202@gmail.com>
2024-04-30 00:05:04 -0400

    Update tools

--- src/data/Skills.jsx
+++ src/data/Skills.jsx
@@ -3,6 +3,6 @@ export default [
         languages: ["Java", "Python", "JavaScript", "TypeScript", "HTML", "CSS"],
         frameworks: ["Next.js", "React.js", "JavaFX", "Tailwind CSS", "Flask", "FastAPI"],
         databases: ["SQLite", "MongoDB"],
-        tools: ["Place Holder", "Place Holder", "Place Holder", "Place Holder", "Place Holder", "Place Holder"],
+        tools: ["Git", "Docker", "Node.js", "GitHub", "Logisim", "Vite"],
     },
 ]
