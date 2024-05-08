d58354a26c0c5ad7c8307f1a1922036f75f6e0bc
Lawrence Li <lawrenceli3202@gmail.com>
2024-05-08 14:02:20 -0400

    Update projects

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -6,21 +6,39 @@ import TravelPlannerImg from "../assets/project/travel-planner.png";
 
 export default [
   {
-    title: "In Progress",
+    title: "JobNow",
     imgURL: "./assets/placeholder.png",
-    stack: ["Place Holder", "Place Holder", "Place Holder"],
-    link: "placeholder",
+    stack: ["React.js", "Flask", "MySQL", "JavaScript", "Tailwind CSS", "Python", "Ant Design", "Vite"],
+    link: "https://github.com/kevkim12/jobnow",
+  },
+  {
+    title: "Travel Planner",
+    imgURL: TravelPlannerImg,
+    stack: ["React.js", "TypeScript", "Tailwind CSS", "Go", "Redis", "Vite",],
+    link: "https://github.com/wjhuang98/travel-planner",
+  },
+  {
+    title: "Space News",
+    imgURL: "./assets/placeholder.png",
+    stack: ["Next.js", "TypeScript", "Ant Design", "Styled-Components"],
+    link: "https://github.com/lawrenceli7/space-news",
+  },
+  {
+    title: "Weather App",
+    imgURL: "./assets/placeholder.png",
+    stack: ["Next.js", "TypeScript", "Tailwind CSS", "Ant Design"],
+    link: "https://github.com/lawrenceli7/weather-app",
   },
   {
     title: "Personal Website",
     imgURL: PortfolioImg,
-    stack: ["Vite", "ReactJS", "Tailwind CSS", "HTML", "JavaScript"],
+    stack: ["React.js", "JavaScript", "Tailwind CSS", "Vite", "HTML",],
     link: "https://github.com/lawrenceli7/portfolio",
   },
   {
     title: "LC3-Processor",
     imgURL: LogisimImg,
-    stack: ["Logisim"],
+    stack: ["Assembly", "Logisim"],
     link: "https://github.com/lawrenceli7/lc3-processor",
   },
   {
@@ -35,19 +53,5 @@ export default [
     stack: ["Java"],
     link: "https://github.com/lawrenceli7/blackjack",
   },
-  {
-    title: "Travel Planner",
-    imgURL: TravelPlannerImg,
-    stack: [
-      "Vite",
-      "ReactJS",
-      "Tailwind CSS",
-      "TypeScript",
-      "HTML",
-      "JavaScript",
-      "Go",
-      "Redis",
-    ],
-    link: "https://github.com/lawrenceli7/travel-planner",
-  },
+
 ];
