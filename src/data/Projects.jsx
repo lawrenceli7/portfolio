9f049333484fc63c2c1161afabd6eefe747f7223
Lawrence Li <lawrenceli3202@gmail.com>
2024-05-09 14:45:17 -0400

    Add images to projects

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,13 +1,16 @@
 import BattleNetworkGameImg from "../assets/project/battle-network-game.png";
 import BlackjackImg from "../assets/project/blackjack.png";
+import JobNowImg from "../assets/project/job-now.png";
 import LogisimImg from "../assets/project/logisim.png";
 import PortfolioImg from "../assets/project/portfolio.png";
+import SpaceNewsImg from "../assets/project/space-news.png";
 import TravelPlannerImg from "../assets/project/travel-planner.png";
+import WeatherAppImg from "../assets/project/weather-app.png";
 
 export default [
   {
     title: "JobNow",
-    imgURL: "./assets/placeholder.png",
+    imgURL: JobNowImg,
     stack: ["React.js", "Flask", "MySQL", "JavaScript", "Tailwind CSS", "Python", "Ant Design", "Vite"],
     link: "https://github.com/kevkim12/jobnow",
   },
@@ -19,13 +22,13 @@ export default [
   },
   {
     title: "Space News",
-    imgURL: "./assets/placeholder.png",
+    imgURL: SpaceNewsImg,
     stack: ["Next.js", "TypeScript", "Ant Design", "Styled-Components"],
     link: "https://github.com/lawrenceli7/space-news",
   },
   {
     title: "Weather App",
-    imgURL: "./assets/placeholder.png",
+    imgURL: WeatherAppImg,
     stack: ["Next.js", "TypeScript", "Tailwind CSS", "Ant Design"],
     link: "https://github.com/lawrenceli7/weather-app",
   },
