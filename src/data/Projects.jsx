bae3b015bc51a48f7ea8163e14760d52987caaf1
Lawrence Li <lawrenceli3202@gmail.com>
2024-05-17 20:18:00 -0400

    Add new project

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -2,7 +2,10 @@ import BattleNetworkGameImg from "../assets/project/battle-network-game.png";
 import BlackjackImg from "../assets/project/blackjack.png";
 import LogisimImg from "../assets/project/logisim.png";
 import PortfolioImg from "../assets/project/portfolio.png";
+import SpaceNewsImg from "../assets/project/space-news.png";
+import SparkBytesImg from "../assets/project/spark-bytes.png";
 import TravelPlannerImg from "../assets/project/travel-planner.png";
+import WeatherAppImg from "../assets/project/weather-app.png";
 
 export default [
   {
@@ -12,6 +15,12 @@ export default [
     link: "https://github.com/kevkim12/jobnow",
   },
   {
+    title: "Spark Bytes",
+    imgURL: SparkBytesImg,
+    stack: ["Next.js", "TypeScript", "Express.js", "JWT", "PostgreSQL", "Prisma", "Ant Design"],
+    link: "https://github.com/lawrenceli7/spark-bytes",
+  },
+  {
     title: "Travel Planner",
     imgURL: TravelPlannerImg,
     stack: ["React.js", "TypeScript", "Tailwind CSS", "Go", "Redis", "Vite",],
@@ -19,13 +28,13 @@ export default [
   },
   {
     title: "Space News",
-    imgURL: "./assets/placeholder.png",
+    imgURL: SpaceNewsImg,
     stack: ["Next.js", "TypeScript", "Ant Design", "Styled-Components"],
     link: "https://github.com/lawrenceli7/space-news",
   },
   {
     title: "Weather App",
-    imgURL: "./assets/placeholder.png",
+    imgURL: WeatherAppImg,
     stack: ["Next.js", "TypeScript", "Tailwind CSS", "Ant Design"],
     link: "https://github.com/lawrenceli7/weather-app",
   },
