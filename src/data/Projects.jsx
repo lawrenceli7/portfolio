fcc552094ed8250050f07d7626c0ce30060f4481
Lawrence Li <lawrenceli3202@gmail.com>
2024-05-17 20:14:03 -0400

    Revert "Add images to projects"
    This reverts commit 9f049333484fc63c2c1161afabd6eefe747f7223.

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,16 +1,13 @@
 import BattleNetworkGameImg from "../assets/project/battle-network-game.png";
 import BlackjackImg from "../assets/project/blackjack.png";
-import JobNowImg from "../assets/project/job-now.png";
 import LogisimImg from "../assets/project/logisim.png";
 import PortfolioImg from "../assets/project/portfolio.png";
-import SpaceNewsImg from "../assets/project/space-news.png";
 import TravelPlannerImg from "../assets/project/travel-planner.png";
-import WeatherAppImg from "../assets/project/weather-app.png";
 
 export default [
   {
     title: "JobNow",
-    imgURL: JobNowImg,
+    imgURL: "./assets/placeholder.png",
     stack: ["React.js", "Flask", "MySQL", "JavaScript", "Tailwind CSS", "Python", "Ant Design", "Vite"],
     link: "https://github.com/kevkim12/jobnow",
   },
@@ -22,13 +19,13 @@ export default [
   },
   {
     title: "Space News",
-    imgURL: SpaceNewsImg,
+    imgURL: "./assets/placeholder.png",
     stack: ["Next.js", "TypeScript", "Ant Design", "Styled-Components"],
     link: "https://github.com/lawrenceli7/space-news",
   },
   {
     title: "Weather App",
-    imgURL: WeatherAppImg,
+    imgURL: "./assets/placeholder.png",
     stack: ["Next.js", "TypeScript", "Tailwind CSS", "Ant Design"],
     link: "https://github.com/lawrenceli7/weather-app",
   },
